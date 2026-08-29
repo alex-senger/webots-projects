0adc36828dda9cf0a7567632adce6ac8b8757d9a
Alex Senger <alex.senger@icloud.com>
2026-08-29 16:14:43 +0900

    feat: add e-puck tutorial controller and world configuration

--- controllers/epuck_go_forward/epuck_go_forward.py
+++ controllers/epuck_go_forward/epuck_go_forward.py
@@ -1,4 +1,4 @@
-from controller import Robot, Motor
+from controller import Robot
 
 TIME_STEP = 64
 
@@ -8,14 +8,14 @@ MAX_SPEED = 6.28
 robot = Robot()
 
 # get a handler to the motors and set target position to infinity (speed control)
-leftMotor = robot.getDevice('left wheel motor')
-rightMotor = robot.getDevice('right wheel motor')
-leftMotor.setPosition(float('inf'))
-rightMotor.setPosition(float('inf'))
+leftMotor = robot.getDevice("left wheel motor")
+rightMotor = robot.getDevice("right wheel motor")
+leftMotor.setPosition(float("inf"))
+rightMotor.setPosition(float("inf"))
 
 # set up the motor speeds at 10% of the MAX_SPEED.
 leftMotor.setVelocity(0.1 * MAX_SPEED)
 rightMotor.setVelocity(0.1 * MAX_SPEED)
 
 while robot.step(TIME_STEP) != -1:
-   pass
+    pass
